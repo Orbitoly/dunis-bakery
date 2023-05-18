d9c9c1e8f5fb58ddb5126814f16029d4eaf16289
Orbitoly <orvirt@gmail.com>
2023-05-18 18:18:30 +0300

    hebrew fixes

--- components/forms/BillingDetails.vue
+++ components/forms/BillingDetails.vue
@@ -10,12 +10,12 @@ const billing = toRef(props, 'modelValue');
   <div class="w-full grid gap-4 lg:grid-cols-2">
     <div class="w-full">
       <label for="first-name">{{ $t('messages.billing.firstName') }}</label>
-      <input v-model="billing.firstName" placeholder="John" type="text" required />
+      <input v-model="billing.firstName" placeholder="דניאלה" type="text" required />
     </div>
 
     <div class="w-full">
       <label for="last-name">{{ $t('messages.billing.lastName') }}</label>
-      <input v-model="billing.lastName" placeholder="Doe" type="text" required />
+      <input v-model="billing.lastName" placeholder="ששון" type="text" required />
     </div>
 
     <div class="w-full">
@@ -30,22 +30,22 @@ const billing = toRef(props, 'modelValue');
 
     <div class="w-full">
       <label for="phone">{{ $t('messages.billing.phone') }}</label>
-      <input v-model="billing.phone" placeholder="+353871234567" type="tel" />
+      <input v-model="billing.phone" placeholder="0524555251" type="tel" />
     </div>
 
     <div class="w-full col-span-full">
       <label for="address1">{{ $t('messages.billing.address1') }}</label>
-      <input v-model="billing.address1" placeholder="O'Connell Street" type="text" required />
+      <input v-model="billing.address1" placeholder="רחוב המסיק" type="text" required />
     </div>
 
     <div class="w-full col-span-full">
       <label for="address2">{{ $t('messages.billing.address2') }}</label>
-      <input v-model="billing.address2" placeholder="Dublin 1" type="text" />
+      <input v-model="billing.address2" placeholder="הגורן 3" type="text" />
     </div>
 
     <div class="w-full">
       <label for="city">{{ $t('messages.billing.city') }}</label>
-      <input v-model="billing.city" placeholder="Dublin" type="text" required />
+      <input v-model="billing.city" placeholder="תל אביב" type="text" required />
     </div>
 
     <div class="w-full">
